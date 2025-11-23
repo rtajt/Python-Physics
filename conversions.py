6398ada90a7d4adb57a5464c84ee9f108e47c157
rtajt <183244240+rtajt@users.noreply.github.com>
2025-11-23 14:14:31 -0500

    Fix conversions.py to conform with DRY + formatting

--- conversions.py
+++ conversions.py
@@ -1,31 +1,38 @@
 import inquirer
 from pint import UnitRegistry
 
-global old_unit, new_unit
 ureg = UnitRegistry()
 
 
 def main():
     questions = [
-        inquirer.List("convert_type", message="What do you want to convert?", choices=["Length", "Area", "Volume", "Time", "Temperature", "Velocity", "Mass", "Force", "Pressure"])
+        inquirer.List("convert_type", message="What do you want to convert?",
+                      choices=["Length", "Area", "Volume", "Time", "Temperature", "Velocity", "Mass", "Force",
+                               "Pressure"])
     ]
     answers = inquirer.prompt(questions)
     selector(answers)
 
+
 def selector(answers):
     choice = answers["convert_type"]
-    match choice:
-        case "Length":
-            length_conversions()
-def length_conversions():
-    global old_unit, new_unit
     questions = [
         inquirer.Text("magnitude", message="What's the magnitude of the quantity you want to convert? (ex: 5, 41)"),
-        inquirer.List("old_units", message="What are the units of it?", choices=["Millimeters", "Centimeters", "Meters", "Kilometers", "Inches", "Feet", "Yards", "Miles"]),
-        inquirer.List("new_units", message="What units do you want to convert to?", choices=["Millimeters", "Centimeters", "Meters", "Kilometers", "Inches", "Feet", "Yards","Miles"])
-    ]
-    answers = inquirer.prompt(questions)
+        inquirer.List("old_units", message="What are the units of it?"),
+        inquirer.List("new_units", message="What units do you want to convert to?"),
 
+    ]
+    match choice:
+        case "Length":
+            questions[1].choices = ["Millimeters", "Centimeters", "Meters", "Kilometers", "Inches", "Feet", "Yards",
+                                    "Miles"]
+            questions[2].choices = ["Millimeters", "Centimeters", "Meters", "Kilometers", "Inches", "Feet", "Yards",
+                                    "Miles"]
+            answers = inquirer.prompt(questions)
+            length_conversions(answers)
+
+
+def length_conversions(answers):
     # Find old unit and new unit and multiply by magnitude
     match answers["old_units"]:
         case "Millimeters":
@@ -44,6 +51,8 @@ def length_conversions():
             old_unit = ureg.yards
         case "Miles":
             old_unit = ureg.miles
+        case _:
+            old_unit = ureg.meters
 
     match answers["new_units"]:
         case "Millimeters":
@@ -63,9 +72,10 @@ def length_conversions():
         case "Miles":
             new_unit = ureg.miles
 
-
     old_quantity = int(answers["magnitude"]) * old_unit
     new_quantity = old_quantity.to(new_unit)
-    print(answers["magnitude"] + " " + answers["old_units"].lower() + " converts to " + str(new_quantity.magnitude) + " " + answers["new_units"].lower())
+    print(answers["magnitude"] + " " + answers["old_units"].lower() + " converts to " + str(
+        new_quantity.magnitude) + " " + answers["new_units"].lower())
+
 
 main()

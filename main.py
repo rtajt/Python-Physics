0bc35dae7aa4e386765c27cee66a7f0d24538b60
rtajt <183244240+rtajt@users.noreply.github.com>
2025-11-23 20:34:41 -0500

    Edit main.py to confirm with pylint

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import conversions
+"""Module for a cleaner prompt + all scripts"""
 import inquirer
+import conversions
 
 def main():
+    """Based on user input, run corresponding script"""
     questions = [
-        inquirer.List("script", message="Which script do you want to run?", choices=["Unit Conversions"])
+        inquirer.List("script", message="Which script do you want to run?",
+        choices=["Unit Conversions"])
     ]
     answers = inquirer.prompt(questions)
     match answers["script"]:

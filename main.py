51406cc0ff1d9c9ab087725f512c6234eee3ef9f
rtajt <183244240+rtajt@users.noreply.github.com>
2025-12-07 14:31:19 -0500

    Reorganize files (will become more useful as more scripts are added

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 """Module for a cleaner prompt + all scripts"""
 import inquirer
-import conversions
+from misc import conversions
+
 
 def main():
     """Based on user input, run corresponding script"""
     questions = [
-        inquirer.List("script", message="Which script do you want to run?",
-        choices=["Unit Conversions"])
+        inquirer.List("script", message="Which type of script do you want to run?",
+        choices=["Misc"])
     ]
     answers = inquirer.prompt(questions)
     match answers["script"]:
-        case "Unit Conversions":
-            conversions.main()
+        case "Misc":
+            questions = [
+                inquirer.List("misc", message="Which misc script do you want to use?",
+                choices=["Conversions"])
+            ]
+            answers = inquirer.prompt(questions)
+            match answers["misc"]:
+                case "Conversions":
+                    conversions.main()
 main()

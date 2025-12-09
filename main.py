65f35f0d5d868ab1b4d5dbd122c3ff56d5a09ce3
rtajt <183244240+rtajt@users.noreply.github.com>
2025-12-09 01:12:50 -0500

    Add error margin script

--- main.py
+++ main.py
@@ -13,12 +13,22 @@ def main():
         case "Misc":
             questions = [
                 inquirer.List("misc", message="Which misc script do you want to use?",
-                choices=["Conversions", "Regression"])
+                choices=["Conversions", "Percent Error", "Regression"])
             ]
             answers = inquirer.prompt(questions)
             match answers["misc"]:
                 case "Conversions":
                     conversions.main()
+                case "Percent Error":
+                    questions = [
+                        inquirer.Text("observed", message="What is the observed value?"),
+                        inquirer.Text("actual", message="What is the actual value?")
+                    ]
+                    answers = inquirer.prompt(questions)
+                    observed = float(answers["observed"])
+                    actual = float(answers["actual"])
+                    error = (abs(observed - actual) / actual) * 100
+                    print(f"There is an error of {error}%")
                 case "Regression":
                     regression.main()
 main()
